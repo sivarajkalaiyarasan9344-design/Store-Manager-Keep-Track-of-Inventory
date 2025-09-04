623ccb7372303fca65b85ac338ed611406f88625
viswaviswa0099-cpu <viswaviswa0099@gmail.com>
2025-09-04 10:49:19 +0530

    Delete NotFound.jsx

--- NotFound.jsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound(){
-    return(
-        <div className="m-2 text-center">
-            <h1>Page Not Found!</h1>
-            <p className="text-8xl">404</p>
-            <p>You may have typed a wrong URL...</p>
-        </div>
-    )
-}
